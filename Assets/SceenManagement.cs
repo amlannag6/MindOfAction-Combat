98566b3d9e9abd033ee7967458c6183a56f69a4d
Amlan Nag <43157787+amlannag6@users.noreply.github.com>
2020-04-03 04:01:52 -0700

    Few level and welcome sceen add

---
+++ Assets/SceenManagement.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceenManagement : MonoBehaviour
+{
+  
+    public void play()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("lvl1");
+    }
+
+   
+}
