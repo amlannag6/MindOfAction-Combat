865ae42fdabda8ac84b6d5032804096c400152c6
Amlan Nag <43157787+amlannag6@users.noreply.github.com>
2020-04-03 04:51:10 -0700

    2 new sceen

--- Assets/SceenManagement.cs
+++ Assets/SceenManagement.cs
@@ -9,6 +9,13 @@ public class SceenManagement : MonoBehaviour
     {
         UnityEngine.SceneManagement.SceneManager.LoadScene("lvl1");
     }
+    public void option()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("options");
+    }
 
-   
+    public void aboutus()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("aboutus");
+    }
 }
