36cb887de7277e76f9de8918e54af7bbc5d04d7c
Amlan Nag <43157787+amlannag6@users.noreply.github.com>
2020-04-04 01:28:14 -0700

    all button set with sound

---
+++ Assets/AudioManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine.Audio;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+
+    public AudioSource myFX;
+    public AudioClip hoverFx;
+    public AudioClip clickFx;
+
+    public void HoverSound()
+    {
+        myFX.PlayOneShot(hoverFx);
+
+    }
+
+    public void clickSound()
+    {
+        myFX.PlayOneShot(clickFx);
+    }
+
+
+
+}
