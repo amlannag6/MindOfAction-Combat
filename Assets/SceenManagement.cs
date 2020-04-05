8a704aa9ceaa5f25bfccaa93d9b35b29bc16a3bd
Amlan Nag <43157787+amlannag6@users.noreply.github.com>
2020-04-04 21:48:34 -0700

    lots of changes with new multiplayer screen

--- Assets/SceenManagement.cs
+++ Assets/SceenManagement.cs
@@ -9,6 +9,11 @@ public class SceenManagement : MonoBehaviour
     {
         UnityEngine.SceneManagement.SceneManager.LoadScene("lvl1");
     }
+    public void multiplayer()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Multiplayer");
+    }
+
     public void option()
     {
         UnityEngine.SceneManagement.SceneManager.LoadScene("options");
