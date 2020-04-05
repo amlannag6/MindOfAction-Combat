44c06e40d42e76e8d9f7f2125c877fdafff14d02
sanjid1339 <63024475+sanjid1339@users.noreply.github.com>
2020-04-05 02:24:52 -0700

    Sum
    Sum

---
+++ Assets/Scripts/Game Manager/UIManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UIManager : MonoBehaviour
+{
+    public static UIManager instance;
+    [SerializeField]
+    TextMeshProUGUI kill_counter;
+    [HideInInspector]
+    public int killCount;
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void UpdateKillCounterUI()
+    {
+        kill_counter.text = killCount.ToString();
+        
+    }
+
+
+}
